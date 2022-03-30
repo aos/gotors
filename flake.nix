4b40e1414d9de36c4238ca131a64f0cf4316e02f
Aos Dabbagh <aosdab@gmail.com>
2022-03-30 01:12:07 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Fast jump between directories";
+
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = "goto";
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              rust-overlay.overlay
+              (self: super: {
+                rustc = self.rust-bin.stable.latest.default;
+                cargo = self.rust-bin.stable.latest.default;
+              })
+            ];
+          };
+        in
+        rec {
+          # nix build
+          defaultPackage = packages.${name};
+          packages = {
+            ${name} = naersk.lib.${system}.buildPackage {
+              pname = name;
+              root = ./.;
+            };
+          };
+
+          # nix develop
+          devShell = pkgs.mkShell {
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = with pkgs; [
+              cargo
+              rust-analyzer
+              rustfmt
+            ];
+          };
+        }
+      );
+}
